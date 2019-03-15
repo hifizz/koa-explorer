5f8dcd581fc9c3e083dd8566755457623a941c32
hifizz <276161014@qq.com>
2019-03-15 16:41:09 +0800

    set cookie is easy

--- index.js
+++ index.js
@@ -2,46 +2,28 @@ const Koa = require('koa');
 const app = new Koa();
 const loggerAsync = require('./middleware/loggerAsync');
 
-const Router = require("koa-router");
-
-let home = new Router()
-
-// 子路由1
-home.get('/', async ( ctx )=>{
-  let html = `
-    <ul>
-      <li><a href="/page/helloworld">/page/helloworld</a></li>
-      <li><a href="/page/404">/page/404</a></li>
-    </ul>
-  `
-  ctx.body = html
-})
-
-// 子路由2
-let page = new Router()
-page.get('/404', async ( ctx )=>{
-  ctx.body = '404 page!'
-}).get('/helloworld', async ( ctx )=>{
-  ctx.body = 'helloworld page!'
-})
-
-// 装载所有子路由
-let router = new Router()
-router.use('/', home.routes(), home.allowedMethods())
-router.use('/page', page.routes(), page.allowedMethods())
-
-// 加载路由中间件
-app.use(router.routes()).use(router.allowedMethods())
-
-// logger
-
 app.use(loggerAsync())
 
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+app.use( async (ctx, next) => {
+  if(ctx.url === '/index') {
+    ctx.cookies.set(
+      "cid",
+      "hello world",
+      {
+        domain: 'localhost',  // 写cookie所在的域名
+        path: '/index',       // 写cookie所在的路径
+        maxAge: 10 * 60 * 1000, // cookie有效时长
+        expires: new Date('2017-02-15'),  // cookie失效时间
+        httpOnly: false,  // 是否只用于http请求中获取
+        overwrite: false  // 是否允许重写
+      }
+    )
+    ctx.body = 'cookie is ok'
+  } else {
+    ctx.body = 'hello world'
+  }
+})
+
 
 app.listen(3000, () => {
   console.log("Your application is starting on 3000, open: ", "http://localhost:3000");
