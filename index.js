c285b821fd5fa2a81e8a14565c5ceef59600a463
hifizz <276161014@qq.com>
2019-03-15 12:03:21 +0800

    get query handle

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
+
 const loggerAsync = require('./middleware/loggerAsync');
-
-const Router = require("koa-router");
-
-let home = new Router()
-
-// 子路由1
-home.get('/', async ( ctx )=>{
-  let html = `
-    <ul>
-      <li><a href="/page/helloworld">/page/helloworld</a></li>
-      <li><a href="/page/404">/page/404</a></li>
-    </ul>
-  `
-  ctx.body = html
-})
-
-// 子路由2
-let page = new Router()
-page.get('/404', async ( ctx )=>{
-  ctx.body = '404 page!'
-}).get('/helloworld', async ( ctx )=>{
-  ctx.body = 'helloworld page!'
-})
-
-// 装载所有子路由
-let router = new Router()
-router.use('/', home.routes(), home.allowedMethods())
-router.use('/page', page.routes(), page.allowedMethods())
-
-// 加载路由中间件
-app.use(router.routes()).use(router.allowedMethods())
-
-// logger
 
 app.use(loggerAsync())
 
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+app.use( async ( ctx ) => {
+  let url = ctx.url
+  // 从上下文的request对象中获取
+  let request = ctx.request
+  let req_query = request.query
+  let req_querystring = request.querystring
+
+  // 从上下文中直接获取
+  let ctx_query = ctx.query
+  let ctx_querystring = ctx.querystring
+
+  ctx.body = JSON.stringify({
+    url,
+    req_query,
+    req_querystring,
+    ctx_query,
+    ctx_querystring
+  })
+})
+
 
 app.listen(3000, () => {
   console.log("Your application is starting on 3000, open: ", "http://localhost:3000");
