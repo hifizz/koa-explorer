b6acf8b5806ed14a4e7dbbe20ea69758c95d62d3
hifizz <276161014@qq.com>
2019-03-15 11:33:22 +0800

    koa-router usage

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const Koa = require('koa');
 const app = new Koa();
-
 const loggerAsync = require('./middleware/loggerAsync');
+
+const Router = require("koa-router");
+
+let home = new Router()
+
+// 子路由1
+home.get('/', async ( ctx )=>{
+  let html = `
+    <ul>
+      <li><a href="/page/helloworld">/page/helloworld</a></li>
+      <li><a href="/page/404">/page/404</a></li>
+    </ul>
+  `
+  ctx.body = html
+})
+
+// 子路由2
+let page = new Router()
+page.get('/404', async ( ctx )=>{
+  ctx.body = '404 page!'
+}).get('/helloworld', async ( ctx )=>{
+  ctx.body = 'helloworld page!'
+})
+
+// 装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
 
 // logger
 
@@ -11,21 +41,6 @@ app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get('X-Response-Time');
   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-
-// x-response-time
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-
-// response
-
-app.use(async ctx => {
-  ctx.body = 'Hello World';
 });
 
 app.listen(3000, () => {
